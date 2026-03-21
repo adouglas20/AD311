e00412e64d317ce9526f664c96d9599414fd0e79
Andy Douglas <andydouglas6416@gmail.com>
2026-03-20 19:24:56 -0700

    completed ESLint Assignment

--- EsLint/eslint.config.mjs
+++ EsLint/eslint.config.mjs
@@ -4,6 +4,32 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js,
+      react: pluginReact
+    },
+    settings: {
+      react: {
+        version: "16"
+      }
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "no-extra-semi": "error",
+      ...pluginReact.configs.flat.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  }
 ]);
